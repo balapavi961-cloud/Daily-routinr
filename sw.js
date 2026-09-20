57e1811253f5e4a39357d65358e400162dccc6da
Balamurugan R <balapavi961@gmail.com>
2026-09-21 01:03:44 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "balamurugan-routine-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.svg", "./icon-512.svg"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return resp;
+  }).catch(() => cached)));
+});
